64bf6f672c9d23b229b76ccd5e62715229b347c0
hasshe94 <hasshe94@kingshigh.school.nz>
2022-02-28 09:15:19 +1300

    2_data_types.py

---
+++ 2_data_types.py
@@ -0,0 +1,66 @@
+print("")
+#1
+print("\nRunaway from me baby Runaway\nRunaway from me baby Runaway\nWhen it starts to get crazy, then run away\nBabe, I got a plan, run away as fast as you can\nRunaway from me baby Runaway\nRunaway from me baby Runaway\nWhen it starts to get crazy\nWhy cant she just runaway?\nBaby I got a plan\nRun away fast as you can")
+#2 Its a tv quote.
+print("\"Jesse we need to cook.\" - Walter White")
+#3
+print("A string is a series of letters and words")
+#4
+print("An integer is a whole number")
+#5
+print("A float a number that is not whole with decimals")
+#6
+print(543/3)
+#7
+print(int(543/3))
+#8
+print(543%3)
+#9
+print(1000%9)
+#10
+print(int(434/10))
+#11
+print(3%2)
+#12
+print(5%5)
+#13
+print(555/2)
+#14
+print(123/2)
+#15
+print(123//2)
+#16
+print(123%2)
+#17
+print(764%2)
+#18
+print(165//2)
+#19
+print(72.2+67.44)
+#20
+print(2+7847875874)
+#21
+print(10-9.9)
+#22
+print(9.9-9)
+#23
+print(7487348/348)
+#24
+print(9339*9.99909)
+#25
+print(999*9)
+#26
+print("Hasan" *10)
+#27
+print("Hasan\n" *10)
+#28
+print("5+10 =" + str(5+10))
+#29
+print("15/2 =" + str(15/2))
+#30
+print("30/2*5 =" + str(30.0/2.0*5.0))
+#31
+print("30/2*5 =" + str(int(30/2*5)))
+
+
+
---
+++ 3_variables.py
@@ -0,0 +1,5 @@
+#1
+player_lives = 3
+#2
+login_name = Hasan Sheikh
+#3
