9ac227032b4fca03426557c3a3bbfd6bfc775268
hasshe94 <hasshe94@kingshigh.school.nz>
2022-02-28 09:48:30 +1300

    3_variables.py

--- 3_variables.py
+++ 3_variables.py
@@ -1,5 +1,42 @@
 #1
 player_lives = 3
 #2
-login_name = Hasan Sheikh
+login_name = "Hasan Sheikh"
 #3
+money_amount = 500
+#4
+user_phonenumber = "020 29839 2383"
+#5
+my_name = "Hasan"
+#6
+my_age = 15
+#7
+current_year = 2022
+#8
+print(my_name)
+#9
+print(my_age)
+#10
+print(current_year)
+#11
+sur_name = "Sheikh"
+#12
+school_name = " Kings High School"
+#13
+favourite_food = "KFC"
+#14
+pet_name = "Mac"
+#15
+blocks_of_chocolate = 44
+#16
+player_score = 22222323
+#17
+player_highscore = 83484878347848
+#18
+number_laps_completed = 323
+#19
+ammunition_left = 54
+#20
+value_current_gear = 928
+#21
+player_current_speed = "999 KPH"
