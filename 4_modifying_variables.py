2a399d324ce30224636897aa8c2f19c16b16f893
hasshe94 <hasshe94@kingshigh.school.nz>
2022-03-02 11:56:26 +1300

    Update 4_modifying_variables.py

---
+++ 4_modifying_variables.py
@@ -0,0 +1,51 @@
+#1
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+#2
+print(money)
+#3
+print("Buying a hat")
+#4
+money = money - hat
+#5
+print("You now have the following amount left over")
+#6
+print(money)
+#7
+print("Buying a top")
+#8
+money = money - top
+#9
+print("You now have the following amount left over")
+#10
+print(money)
+#11
+print("Buying a pants")
+#12
+money = money - pants
+#13
+print("You now have the following amount left over")
+#14
+print(money)
+#15
+print("Buying a belt")
+#16
+money = money - belt
+#17
+print("You now have the following amount left over")
+#18
+print(money)
+#19
+print("Buying a shoes")
+#20
+money = money - shoes
+#21
+print("You now have the following amount left over")
+#22
+print(money)
+
+
