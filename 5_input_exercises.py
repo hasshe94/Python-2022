a2304f2b0a2b83c78b5cf59a3a6f039a093784c3
hasshe94 <hasshe94@kingshigh.school.nz>
2022-03-03 13:23:49 +1300

    my 5_input_exercises so far

--- 5_input_exercises.py
+++ 5_input_exercises.py
@@ -1,7 +1,33 @@
 #1
-input_1 = input("Press any key")
+input("Press any key.\n")
 #2
-user_name = input("What is your name?")
+input("What is your name?\n")
 #3
-user_age = input("What is your age?")
+input("What is your age?\n")
 #4
+user_name = input("What is your name?\n")
+#5
+user_age = int(input("What is your age?\n"))
+#6
+fave_movie = ("What is your favorutie movie?\n")
+#7
+book_name = ("Name a book.\n")
+#8
+adjective_name = ("Give an adjective.\n")
+#9
+noun_name = ("Give a noun.\n")
+#10
+verb_name = ("Give a verb.\n")
+#11
+print(f"Hello {user_name} welcome to my program.\n")
+print(f"You are {user_age} years old which means in 10 years you will be {user_age + 10} years old!\n")
+print(f"Your favourite movie which is {fave_movie} is a pretty decent movie and a good choice!\n")
+print(f"{book_name} is a pretty bad book not gonna lie.\n")
+print(f"The adjective you chose, {adjective_name} perfectly describes you!!!!\n")
+print(f" {noun_name} is a pretty weird thing.\n")
+print(f"The verb you chose, {verb_name} is exactly what im doing right now.\n")
+#12
+user_age1 = int(input("What is your age?\n")
+print(f"In 10 years you will be {user_age1 + 10} years old.\n")
+#13
+print(f"You were born in {2022 - user_age1}.\n")
